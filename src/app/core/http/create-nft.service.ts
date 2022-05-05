743a352ec5374393a44a52ca34042c1e9a14e33f
Pintu Das <rex@workwithme.in>
2022-05-05 19:32:11 +0530

    create NFT ready

--- src/app/core/http/create-nft.service.ts
+++ src/app/core/http/create-nft.service.ts
@@ -1,44 +1,57 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse, HttpEvent, HttpEventType } from '@angular/common/http';
-import { Observable, throwError } from "rxjs";
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpEvent,
+  HttpEventType,
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
-import { environment } from "src/environments/environment";
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CreateNftService {
   private _url: string;
+  public progress: number;
   constructor(private _http: HttpClient) {
     this._url = environment.url;
+    this.progress = 1;
   }
 
   public createNft(formData: any): Observable<any> {
-    return this._http.post<any>(`${this._url}/createNft`, formData, {
-      reportProgress: true,
-      observe: 'events',
-      responseType: 'json'
-    }).pipe(map(event => this.getEventMessage(event, formData)),
-      catchError(this.handleError));
+    return this._http
+      .post<any>(`${this._url}/createNft`, formData, {
+        reportProgress: true,
+        observe: 'events',
+        responseType: 'json',
+      })
+      .pipe(
+        map((event): any => this.getEventMessage(event, formData)),
+        catchError(this.handleError)
+      );
   }
 
-  private getEventMessage(event: HttpEvent<any>, formData: any) {
+  private getEventMessage(event: any, formData: any) {
     switch (event.type) {
       case HttpEventType.UploadProgress:
         return this.fileUploadProgress(event);
       case HttpEventType.Response:
         return this.apiResponse(event);
       default:
-        return `File "${formData.get('file')}" surprising upload event: ${event.type}.`;
+        return `File "${formData.get('file')}" surprising upload event: ${
+          event.type
+        }.`;
     }
   }
 
   private fileUploadProgress(event: any) {
-    const percentDone = Math.round(100 * event.loaded / event.total);
+    const percentDone = Math.round((100 * event.loaded) / event.total);
     return { status: 'progress', message: percentDone };
   }
   private apiResponse(event: any) {
-    return event.body;
+    return { ...event.body, status: 'success' };
   }
   private handleError(error: HttpErrorResponse) {
     if (error.error instanceof ErrorEvent) {
@@ -48,11 +61,10 @@ export class CreateNftService {
       // The backend returned an unsuccessful response code.
       // The response body may contain clues as to what went wrong,
       console.error(
-        `Backend returned code ${error.status}, ` +
-        `body was: ${error.error}`);
+        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
+      );
     }
     // return an observable with a user-facing error message
-    return throwError(
-      'Something bad happened; please try again later.');
+    return throwError('Something bad happened; please try again later.');
   }
 }
--- src/app/modules/authentication/components/create-nft-page/create-nft-page.component.ts
+++ src/app/modules/authentication/components/create-nft-page/create-nft-page.component.ts
@@ -2,27 +2,42 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { DomSanitizer } from '@angular/platform-browser';
 import { CreateNftService } from 'src/app/core/http/create-nft.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-create-nft-page',
   templateUrl: './create-nft-page.component.html',
-  styleUrls: ['./create-nft-page.component.scss']
+  styleUrls: ['./create-nft-page.component.scss'],
 })
 export class CreateNftPageComponent implements OnInit {
   // apiKey: string | null = '';
-  authorizationKey: string = 'f4366928-a6a2-41a0-ad40-d80805528132';
-  mintTo: string = '0xbA7356120E28D862f6B0528900F6Af2dF08D6E34';
+  authorizationKey: string | null = 'AUTHORIZATION_KEY';
+  name: string | null = 'FILE_NAME';
+  description: string | null = 'DESCRIPTION';
+  mintTo: string = 'WALLET_ADDRESS';
   image: any = 'assets/images/Sample-image.svg';
+  url: string = environment.url;
+  defaultCurl: string = '';
   createNftForm!: FormGroup;
   fileUpload = { status: '', message: '', filePath: '' };
+  response: any;
   constructor(
     private formBuilder: FormBuilder,
     private domSanitizer: DomSanitizer,
-    private createNftService: CreateNftService,
-  ) {}
+    private createNftService: CreateNftService
+  ) {
+    this.defaultCurl = `curl --location --request POST '${environment.url}' \
+    --header 'Content-Type: application/json' \
+    --form 'file=@"/path-to-your-file/${this.name}"' \
+    --form 'mintTo="${this.mintTo}"' \
+    --form 'name="wallet"' \
+    --form 'description="${this.description}"' \
+    --form 'authorizationKey="${this.authorizationKey}"'`;
+  }
 
   ngOnInit(): void {
     // this.apiKey = localStorage.getItem("api_key");
+    this.response = null;
     this.createNftForm = this.formBuilder.group({
       name: [''],
       file: [''],
@@ -31,19 +46,22 @@ export class CreateNftPageComponent implements OnInit {
       authorizationKey: [''],
     });
   }
- 
+
   onSelectedFile(event: any) {
     if (event.target.files.length > 0) {
       const file = event.target.files[0];
-      console.log(file);
+      console.log(event.target.files);
       var reader = new FileReader();
 
       reader.readAsDataURL(event.target.files[0]); // read file as data url
 
-      reader.onload = (event: any) => { // called once readAsDataURL is completed
-        this.image = this.domSanitizer.bypassSecurityTrustUrl(event.target.result);
-      }
-      
+      reader.onload = (event: any) => {
+        // called once readAsDataURL is completed
+        this.image = this.domSanitizer.bypassSecurityTrustUrl(
+          event.target.result
+        );
+      };
+
       this.createNftForm.get('file')?.setValue(file);
       this.createNftForm.get('name')?.setValue(file.name);
     }
@@ -54,22 +72,51 @@ export class CreateNftPageComponent implements OnInit {
     formData.append('name', this.createNftForm.get('name')?.value);
     formData.append('file', this.createNftForm.get('file')?.value);
     formData.append('mintTo', this.createNftForm.get('mintTo')?.value);
-    formData.append('description', this.createNftForm.get('description')?.value);
-    formData.append('authorizationKey', this.createNftForm.get('authorizationKey')?.value);
-    console.log('name:', formData.get('name'));
-    console.log('file:', formData.get('file'));
-    console.log('mintTo:', formData.get('mintTo'));
-    console.log('authorizationKey:', formData.get('authorizationKey'));
-    console.log('description:', formData.get('description'));
+    formData.append(
+      'description',
+      this.createNftForm.get('description')?.value
+    );
+    formData.append(
+      'authorizationKey',
+      this.createNftForm.get('authorizationKey')?.value
+    );
     this.createNftService.createNft(formData).subscribe(
       (res: any) => {
-        console.log(res);
-        this.fileUpload = res;
-        return this.fileUpload;
-      }, (err: any) => {
+        if (res.status === 'success') {
+          this.response = JSON.stringify(res);
+        } else {
+          this.fileUpload = res;
+        }
+      },
+      (err: any) => {
         return err;
       }
     );
   }
-
+  onChangeMintTo(event: string) {
+    this.mintTo = event;
+    this.defaultCurl = this.defaultCurlGenerator();
+  }
+  onChangeDescription(event: string) {
+    this.description = event;
+    this.defaultCurl = this.defaultCurlGenerator();
+  }
+  onChangeName(event: string) {
+    this.name = event;
+    this.defaultCurl = this.defaultCurlGenerator();
+  }
+  onChangeAuthorizationKey(event: string) {
+    this.authorizationKey = event;
+    this.defaultCurl = this.defaultCurlGenerator();
+  }
+  defaultCurlGenerator() {
+    const curl = `curl --location --request POST '${this.url}' \
+    --header 'Content-Type: application/json' \
+    --form 'file=@"/path-to-your-file/${this.name}"' \
+    --form 'mintTo="${this.mintTo}"' \
+    --form 'name="wallet"' \
+    --form 'description="${this.description}"' \
+    --form 'authorizationKey="${this.authorizationKey}"'`;
+    return curl;
+  }
 }
