b5053dfcb7a39d77e581850c2628bab008a9db75
Pintu Das <rex@workwithme.in>
2022-05-16 17:10:20 +0530

    Google Analytics added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from '@angular/cdk/layout';
+import { NgxGoogleAnalyticsModule, NgxGoogleAnalyticsRouterModule } from 'ngx-google-analytics';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthenticationModule } from './modules/authentication/authentication.module';
@@ -24,6 +25,8 @@ import { FormsModule } from '@angular/forms';
     StaticModule,
     SharedModule,
     AuthenticationModule,
+    NgxGoogleAnalyticsModule.forRoot('G-TY55466GK8'),
+    NgxGoogleAnalyticsRouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
