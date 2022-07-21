9b96da4091771d89db0182a66c7824f931a1d74d
Pintu Das <rex@workwithme.in>
2022-07-21 19:37:23 +0530

    calendly popup added at landing page

--- src/app/modules/static/components/landing/landing.component.ts
+++ src/app/modules/static/components/landing/landing.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+declare global {
+  interface Window {
+     Calendly: any;
+  }
+}
+
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent implements OnInit {
-
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  talkToAnExpert() {
+    window.Calendly.initPopupWidget({
+      url: 'https://calendly.com/shyft-to',
+      parentElement: document.querySelector('.calendly-popup-widget'),
+      prefill: {}
+   });
+  }
 }
